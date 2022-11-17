c13878781869c436b198cabfa0a48eafc5982435
BuildTools <unconfigured@null.spigotmc.org>
2022-11-17 17:01:36 +0100

    Ändrad config

--- libGDX (spel)/desktop/src/com/mygdx/game/DesktopLauncher.java
+++ libGDX (spel)/desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Graphics.DisplayMode;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.mygdx.game.MyGdxGame;
@@ -8,8 +9,10 @@ import com.mygdx.game.MyGdxGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setWindowedMode(800, 480);
 		config.setForegroundFPS(60);
 		config.setTitle("Epic Game");
+		config.useVsync(true);
 		new Lwjgl3Application(new MyGdxGame(), config);
 	}
 }
